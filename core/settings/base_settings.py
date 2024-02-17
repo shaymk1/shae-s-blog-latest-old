60275dc297c789058ad3f9203babe87c31001c3a
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2024-02-17 18:20:15 +0200

    setting up different enviroments using .env

--- core/settings/base_settings.py
+++ core/settings/base_settings.py
@@ -1,6 +1,10 @@
 
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%^bsl4=()b0_b6%p2-w#0w#nb7+vmzn3x2qwj_v@3n_92s_zcz'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
