2f5a8a26db1e34ddf69df625684b4179c1705081
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2024-02-17 20:44:55 +0200

    added static files, added index.html, css, footer, navbar and base

--- core/settings/base_settings.py
+++ core/settings/base_settings.py
@@ -49,7 +49,8 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        # 'DIRS': [BASE_DIR / 'templates'],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
