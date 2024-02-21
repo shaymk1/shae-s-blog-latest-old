28a564239a5912c68ac5b2761edfe9edb131f071
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2024-02-21 13:58:28 +0200

    modified models and also created a new Tags model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,46 @@
+from unicodedata import category
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    options = (
+        ("draft", "Draft"),
+        ("published", "Published"),
+    )
+    image = models.ImageField(
+        null=True, blank=True, 
+        upload_to="articles",
+        default="placeholder.png"
+        )
+    category = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    time_required_to_read = models.CharField(
+        max_length=250, 
+        default="2 Min Read"
+        )
+    title = models.CharField(max_length=250)
+    # slug = models.SlugField(max_length=250, unique=True)
+    intro = models.TextField(blank=True)
+    content = models.TextField(blank=True)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="post_author"
+    )
+    author_image = models.ImageField(
+         null=True, blank=True, 
+         upload_to= "articles",
+         default="placeholder.png")
+
+    class Meta:
+        ordering = ("-created_at",)  # decending order
+
+    def __str__(self):
+        return self.title
+    
+
+class Tags(models.Model):
+    tag = models.CharField(max_length=250)
+    
+    def __str__(self):
+        return self.tag
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from .models import Post, Tags
 
 
 def home(request):
-    return render(request, "blog/index.html")
+    model = Post
+    posts = Post.objects.all().order_by("created_at")
+    tags = Tags.objects.all()
+    context = {
+        "model": model,
+        "posts": posts,
+        "tags": tags
+
+    }
+    return render(request, "blog/index.html", context)
+
+
+# def trending_tags(request):
+#     model = Tags
+#     tags = Tags.objects.all()
+#     context = {
+#         "model": model,
+#         "tags": tags
+#     }
+#     return render(request, "blog/index.html", context)
